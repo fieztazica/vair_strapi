d78b946139a0c8b5c1d320de493ae14379805ae3
Dat Hoang <fiezt@outlook.com>
2023-11-20 21:20:55 +0700

    env URL refractor

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,11 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-  url: 'https://strapi.vair.nyte.tk'
-});
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+        keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+        populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+    url: env('URL', 'https://strapi.vair.nyte.tk'),
+})
